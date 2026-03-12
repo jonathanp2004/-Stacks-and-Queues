26da06b762d10bb7453a4847fd219edcce85135a
jonathanp2004 <jpadilla1782.2@gmail.com>
2026-03-12 15:14:32 -0500

    Changed line 14 to list.add(0,c), since linkedList inherently place new item at end of linked list, which caused wrong result when testing. By making change we added the character c to the front which in turn allowed the linked list to be used a proper Stack ADT.

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -11,7 +11,7 @@ public String isBalanced(String s){
     for (int i = 0; i < s.length() ; i++) {
         char c = s.charAt(i);
         if(c == '(' || c == '{' || c== '[' || c == '<' || c == '¿'){
-            list.add(c);
+            list.add(0,c);
         } else if(c == ')' || c == '}' || c== ']' || c == '>' || c == '?'){
             if(list.isEmpty()){
                 return "NO";
@@ -36,8 +36,18 @@ public String isBalanced(String s){
 public static void main(String[] args){
     Main main = new Main();
 
-    String =
+    String s = "{{[[(()<>)]]}}";
+    String v = "{[(])}";
+    String h = "{[()]}";
 
+    String result = main.isBalanced(s);
+    String result1 = main.isBalanced(v) ;
+    String result2 = main.isBalanced(h);
+
+
+    System.out.println(result);
+    System.out.println(result1);
+    System.out.println(result2);
 }
 
 }
